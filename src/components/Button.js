566b7fd7b2192a079b5119e78c2e51d8d8b6806a
Korey <smithrkorey@gmail.com>
2021-03-07 20:54:27 -0700

    edits

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,30 +7,27 @@ const SIZES = ['btn--medium', 'btn--large'];
 
 
 export const Button = ({
-    children, 
-    type, 
-    conClick, 
-    buttonStyle, 
+    children,
+    type,
+    onClick,
+    buttonStyle,
     buttonSize
-}) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) 
-    ? buttonStyle 
-    : STYLES[0]
-
+  }) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+      ? buttonStyle
+      : STYLES[0];
+  
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
-
+  
     return (
-        <Link to='/Contact-me' className='btn-mobile'>
-            <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onClick}
-            type={type}
-
-            >
-                {children}
-            </button>
-
-        </Link>
-    )
-
-};
+      <Link to='/sign-up' className='btn-mobile'>
+        <button
+          className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+          onClick={onClick}
+          type={type}
+        >
+          {children}
+        </button>
+      </Link>
+    );
+  };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,79 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
 import { Link } from 'react-router-dom';
-
+import './Navbar.css';
 
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
     }
+  };
 
-    return (
-        <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link to="/" className='navbar-logo'>
-                KoreySmith <i class="fas fa-code"></i>
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/About' className='nav-links' onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/Work' className='nav-links' onClick={closeMobileMenu}>
-                                Work
-                            </Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/Contact-Me' className='nav-links-mobile' onClick={closeMobileMenu}>
-                                Contact Me
-                            </Link>
-                        </li>
-                </ul>
-                {button && <Button buttonStyle='btn--outline'>Contact Me</Button>}
-            </div>
-        </nav>
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            Korey Smith
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/about'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link
+                to='/work'
+                className='nav-links'
+                onClick={closeMobileMenu}
+              >
+                Work
+              </Link>
+            </li>
+
+            <li>
+              <Link
+                to='/contact'
+                className='nav-links-mobile'
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>Contact</Button>}
+        </div>
+      </nav>
     </>
-    )
+  );
 }
 
-export default Navbar
+export default Navbar;
